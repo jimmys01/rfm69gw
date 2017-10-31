6f2b24c4144eba5d68fd8fc33cf3b419266d69e1
Xose Pérez <xose.perez@gmail.com>
2017-10-31 07:42:02 +0100

    Whola lotta changes

---
+++ esp8266/gateway/src/config/prototypes.h
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <NtpClientLib.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncMqttClient.h>
+#include "RFM69Manager.h"
+
+template<typename T> bool setSetting(const String& key, T value);
+template<typename T> bool setSetting(const String& key, unsigned int index, T value);
+template<typename T> String getSetting(const String& key, T defaultValue);
+template<typename T> String getSetting(const String& key, unsigned int index, T defaultValue);
+
+char * ltrim(char * s);

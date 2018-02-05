a01ff56228af5f2dd56ce4b73913e49eed270091
Xose Pérez <xose.perez@gmail.com>
2018-02-05 11:36:21 +0100

    Fix unmet dependency (#6)

--- esp8266/gateway/src/config/prototypes.h
+++ esp8266/gateway/src/config/prototypes.h
@@ -5,6 +5,10 @@
 #include <AsyncMqttClient.h>
 #include "RFM69Manager.h"
 
+extern "C" {
+    #include "user_interface.h"
+}
+
 template<typename T> bool setSetting(const String& key, T value);
 template<typename T> bool setSetting(const String& key, unsigned int index, T value);
 template<typename T> String getSetting(const String& key, T defaultValue);
